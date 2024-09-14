39670a20536e646d5f267085bcc33210e6f6bdf0
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2024-09-14 12:49:50 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-uvicorn main:app --host '0.0.0.0' --port 8000 --reload --reload-include '*.ini' &
+uvicorn main:app --host '::' --port 8000 --reload --reload-include '*.ini' &
   
 # Wait for any process to exit
 wait -n
